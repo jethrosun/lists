aa5cf1ac4b7f46eaabbabf3ef2a46933da8bae88
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-01-16 14:50:23 -0500

    Update:  ok

--- src/second.rs
+++ src/second.rs
@@ -1,14 +1,45 @@
 //use std::mem;
+//use crate::second::Iterator;
 
 pub struct List<T> {
     head: Link<T>,
 }
 
+/// A short way to implement Link.
+///
+/// A previous example is:
+/// ```
+/// ```
 type Link<T> = Option<Box<Node<T>>>;
 
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+/// IntoIter is a type just wrapper around List.
+pub struct IntoIter<T>(List<T>);
+
+// Iter is generic over *some* lifetime, it doesn't care
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T: 'a> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        return IntoIter(self);
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        return self.0.pop();
+    }
 }
 
 impl<T> List<T> {
@@ -56,6 +87,69 @@ impl<T> Drop for List<T> {
     }
 }
 
+// No lifetime here, List doesn't have any associated lifetimes
+impl<T> List<T> {
+    /// Note that this is syntax sugar for
+    /// ```ignore
+    /// impl<T> List<T> {
+    ///     pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+    ///         Iter { next: self.head.as_ref().map(|node| &**node)
+    ///     }
+    /// }
+    /// ```
+    // We declare a fresh lifetime here for the *exact* borrow that
+    // creates the iter. Now &self needs to be valid as long as the
+    // Iter is around.
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_ref().map(|node| &**node),
+        }
+    }
+}
+
+/// This is syntax sugar for:
+/// ```ignore
+/// impl<'a, T> Iterator for Iter<'a, T> {
+///     type Item = &'a T;
+///
+///     fn next<'b>(&'b mut self) -> Option<&'a T> { /* stuff */ }
+/// }
+/// ```
+
+// *Do* have a lifetime here, because Iter does have an associated lifetime
+impl<'a, T> Iterator for Iter<'a, T> {
+    // Need it here too, this is a type declaration
+    type Item = &'a T;
+
+    // None of this needs to change, handled by the above.
+    // Self continues to be incredibly hype and amazing
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
+    }
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_mut().map(|node| &mut **node),
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -96,6 +190,45 @@ mod test {
 
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter_mut = list.iter_mut();
+        assert_eq!(iter_mut.next(), Some(&mut 3));
+        assert_eq!(iter_mut.next(), Some(&mut 2));
+        assert_eq!(iter_mut.next(), Some(&mut 1));
     }
 
 }
